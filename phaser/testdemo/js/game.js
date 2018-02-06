7d3c28ecbaa3972b11ddc772b879ce06960d246a
yangjie <yangjie26@jd.com>
2018-02-06 20:54:18 +0800

    上传phaser

---
+++ phaser/testdemo/js/game.js
@@ -0,0 +1,18 @@
+BasicGame.Game = function (game) {
+
+}
+
+BasicGame.Game.prototype = {
+    preload: function () {
+        console.log('preload');
+    },
+    create: function () {
+        console.log('create');
+    },
+    update: function () {
+        console.log('update');
+    },
+    render: function () {
+    }
+
+}
